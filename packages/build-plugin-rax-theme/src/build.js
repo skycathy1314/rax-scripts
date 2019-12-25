c63195b2a5bd22dc8fe5bc356c962503b9c4e1ab
yacheng.sz <yacheng.sz@taobao.com>
2019-12-25 21:16:50 +0800

    feat: init theme plugin

---
+++ packages/build-plugin-rax-theme/src/build.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+const consoleClear = require("console-clear");
+const { handleWebpackErr } = require("rax-compile-config");
+module.exports = (api, options = {}) => {
+  onHook("after.build.compile", async ({ err, stats }) => {
+  });
+};
---
+++ packages/build-plugin-rax-theme/src/dev.js
@@ -0,0 +1,9 @@
+const consoleClear = require("console-clear");
+const chalk = require("chalk");
+const { handleWebpackErr } = require("rax-compile-config");
+
+module.exports = (api, options = {}) => {
+  onHook("after.start.compile", async args => {
+    
+  });
+};
