478c6cf4602f2ebc5f3597f909fc4368599dbabe
yacheng.sz <yacheng.sz@taobao.com>
2019-12-26 21:31:37 +0800

    feat: use theme config

--- packages/build-plugin-rax-theme/src/build.js
+++ packages/build-plugin-rax-theme/src/build.js
@@ -2,6 +2,7 @@ const chalk = require("chalk");
 const consoleClear = require("console-clear");
 const { handleWebpackErr } = require("rax-compile-config");
 module.exports = (api, options = {}) => {
+  const {onHook, onGetWebpackConfig} = api;
   onHook("after.build.compile", async ({ err, stats }) => {
   });
 };
--- packages/build-plugin-rax-theme/src/dev.js
+++ packages/build-plugin-rax-theme/src/dev.js
@@ -3,7 +3,35 @@ const chalk = require("chalk");
 const { handleWebpackErr } = require("rax-compile-config");
 
 module.exports = (api, options = {}) => {
-  onHook("after.start.compile", async args => {
-    
+  const {onHook, onGetWebpackConfig} = api;
+
+	onGetWebpackConfig((config) => {
+    console.log('theme true');
+
+    config.module
+      .rule('css')
+      .test(/\.css?$/)
+      .use('css')
+      .loader(require.resolve('stylesheet-loader'))
+      .options({
+        theme: true
+      });
+
+    config.module
+      .rule('less')
+      .test(/\.less?$/)
+      .use('css')
+      .loader(require.resolve('stylesheet-loader'))
+      .end()
+      .use('less')
+      .loader(require.resolve('less-loader'))
+      .options({
+        theme: true
+      });
+
+  });
+
+  onHook("before.start.load", async args => {
+    console.log("before.start.load", args);
   });
 };
